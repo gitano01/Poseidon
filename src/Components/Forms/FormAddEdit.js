2e7d08cb860b8e6c2c9f54807f11744f2ffe3cab
Victor <vramirez.info@gmail.com>
2019-09-11 19:33:20 -0500

    Modificacion de formulario de nuevo usuario

--- src/Components/Forms/FormAddEdit.js
+++ src/Components/Forms/FormAddEdit.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import {Form, FormGroup, Label, Input } from 'reactstrap';
-import {FaUser, FaKey} from "react-icons/all";
+import {FaUser, FaKey, FaEnvelope} from "react-icons/all";
 
 class AddEditForm extends React.Component {
-/*    state = {
-        id: 0,
-        first: '',
-        last: '',
-        email: '',
-        phone: '',
-        location: '',
-        hobby: ''
-    }*/
 
     constructor(props){
         super(props);
         this.state = {
             id: 0,
             username:'',
+            email:'',
             password:'',
             password_confirm:''
         }
@@ -27,9 +19,26 @@ class AddEditForm extends React.Component {
 
     }
 
+
+
+    ChecaInput = () => {
+    const  {username} = this.state;
+    const {email} = this.state;
+    const  {password} = this.state;
+    const  {password_confirm} = this.state;
+    var op =0;
+
+    username.toString() === "" ? alert("","el usuario no puede estar vacío")
+        : email.toString() ==="" ? alert("","el email no puede estar vacío")
+        : password.toString() ==="" ? alert("","La contraseña no puede estar vacía")
+        : password_confirm.toString() ==="" ? alert("","Confirmar contraseña no puede estar vacío")
+        : password_confirm.toString() !== password.toString() ? alert("","Las contraseñas no coinciden")
+        : this.submitFormAdd()
+       }
+
     submitFormAdd= e =>{
         e.preventDefault()
-        fetch('http://localhost:3000/crud',{
+        fetch('http://localhost:3000/api/users/new',{
             method:'post',
             headers:{
                 'Content-Type':'application/json'
@@ -50,10 +59,9 @@ class AddEditForm extends React.Component {
             })
     }
 
-
     submitFormEdit = e => {
         e.preventDefault()
-        fetch('http://localhost:3000/crud', {
+        fetch('http://localhost:3000/api/newUser', {
             method: 'put',
             headers: {
                 'Content-Type': 'application/json'
@@ -98,6 +106,12 @@ class AddEditForm extends React.Component {
                 </FormGroup>
                 <FormGroup className="input-group form-group">
                     <div className="input-group-prepend">
+                        <span className="input-group-text"><FaEnvelope/></span>
+                    </div>
+                    <Input type="text" placeholder="correo electrónico" name="email" id="email" onChange={this.onChange} value={this.state.username === null ? '' : this.state.username} />
+                </FormGroup>
+                <FormGroup className="input-group form-group">
+                    <div className="input-group-prepend">
                     <span className="input-group-text"><FaKey/></span>
                     </div>
                     <Input type="password" placeholder="Contraseña" name="password" id="password" onChange={this.onChange} value={this.state.last === null ? '' : this.state.last}  />
@@ -110,7 +124,7 @@ class AddEditForm extends React.Component {
                 </FormGroup>
 
                 <div className="form-group ">
-                    <input className="form-control col-md-12 btn-danger" value="Registrar" type="submit"/>
+                    <input className="form-control col-md-12 btn-danger" value="Registrar" type="submit" onClick={this.ChecaInput}/>
                 </div>
             </Form>
         );
